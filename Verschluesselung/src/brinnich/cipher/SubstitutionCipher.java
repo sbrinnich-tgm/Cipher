a5598973a549689c967494bb6671638c72991047
Brinnich Selina <sbrinnich@student.tgm.ac.at>
2014-04-04 15:09:31 +0200

    Created class SubstitutionCipher

---
+++ Verschluesselung/src/brinnich/cipher/SubstitutionCipher.java
@@ -0,0 +1,21 @@
+package brinnich.cipher;
+
+public class SubstitutionCipher extends MonoAlphabeticCipher{
+	
+	/**
+	 * Konstruktor, der das Geheimalphabet setzt
+	 * @param secretAlphabet der String, auf den das Geheimalphabet gesetzt werden soll
+	 */
+	public SubstitutionCipher(String secretAlphabet){
+		this.setSecretAlphabet(secretAlphabet);
+	}
+	
+	/**
+	 * Setzt das Geheimalphabet auf einen bestimmten Wert
+	 * @param secretAlphabet der String, auf den das Geheimalphabet gesetzt werden soll
+	 */
+	public void setSecretAlphabet(String secretAlphabet){
+		this.setSecretAlphabet(secretAlphabet);
+	}
+	
+}
